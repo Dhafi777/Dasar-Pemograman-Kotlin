dce4f6ff4a38e80b94ef34f40aad3232ce2c9101
Dhafi777 <ininama.404@gmail.com>
2024-09-03 13:45:52 +0700

    Add Var vs Val.kt

---
+++ src/main/kotlin/Var vs Val.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 //nilai pertama pada vaVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //nilai kedua pada varVariabel
+    println("Pemberian nilai kedua pada varVariabel = " + varVariabel)
+}
